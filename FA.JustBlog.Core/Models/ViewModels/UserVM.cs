509490c0dcf5743dd3cc1ea8bd5a765dac3e8a5f
TMT4869 <hao.nn193294@sis.hust.edu.vn>
2024-04-12 22:16:42 +0700

    Create user view model

---
+++ FA.JustBlog.Core/Models/ViewModels/UserVM.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FA.JustBlog.Core.Models.ViewModels
+{
+    public class UserVM
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+
+        public int? Age { get; set; }
+
+        [MaxLength(750, ErrorMessage = "About must be less than 751 characters")]
+        public string? About { get; set; }
+
+        [Required(ErrorMessage = "Role is required")]
+        public List<string>? Roles { get; set; }
+    }
+}
